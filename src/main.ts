b2ad5ab22e3895267ea7e76190d9f756d65843de
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2025-10-29 01:40:54 -0400

    cambios

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'], // o '*' si quieres permitir todos los orígenes
+    origin: '*', // o '*' si quieres permitir todos los orígenes
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(process.env.PORT ?? 3000);
